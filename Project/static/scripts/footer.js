e483fee755973a52e966579022df37272165f5e7
Aidan La Penta <alape632@live.kutztown.edu>
2024-03-24 23:01:19 -0400

    Added copyright

--- Project/static/scripts/footer.js
+++ Project/static/scripts/footer.js
@@ -1,13 +1,14 @@
 function replacePlaceholderFooter() {
     var footerPlaceholder = document.getElementById('footerPlaceholder');
 
-    // Design the navbar
+    //  var footerContent = '<footer class="site-footer">' +
+    //                     '   <h2 class="footer-nav-heading">Family Frame ©2023<h2>' +
+    //                     '</footer>';
     var footerContent = '<footer class="site-footer">' +
-                        '   <h2 class="footer-nav-heading">Family Frame<h2>' +
+                        '   <h2 class="footer-nav-heading">Family Frame ©2023<h2>' +
                         '   <ul>' +
                         '       <li><a href="#"onclick="navigateToAbout()">About</a></li>' +
                         '   </ul>' +
                         '</footer>';
-    // Replace the innerHTML with dynamic content
     footerPlaceholder.innerHTML = footerContent;
 }
