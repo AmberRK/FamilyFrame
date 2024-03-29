f34a39ac64224afecc5fa34a9c171068e8c72b0f
Aidan La Penta <alape632@live.kutztown.edu>
2024-03-29 02:06:08 -0400

    Added d3 fragment

--- Project/static/scripts/main.js
+++ Project/static/scripts/main.js
@@ -1,4 +1,4 @@
-// import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+// import * as d3 from "d3";
 
 // Grabs the login cookie
 document.addEventListener('DOMContentLoaded', async () => {
