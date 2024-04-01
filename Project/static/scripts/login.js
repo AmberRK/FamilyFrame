0ce648803ae03dcf480a3838134e40eee3c47c07
Amber Kulp <akulp740@live.kutztown.edu>
2024-04-01 10:06:48 -0400

    myTrees becomes available when logging in

--- Project/static/scripts/login.js
+++ Project/static/scripts/login.js
@@ -53,4 +53,78 @@ async function loggedIn()
     console.error('Error:', error);
     throw error;
   }
+
+  replacePlaceholderNavbar();
+}
+
+async function replacePlaceholderNavbar() {
+  var navbarPlaceholder = document.getElementById('navbarPlaceholder');
+  
+  let clickable = await checkLoggedIn();
+  console.log(clickable);
+  var navbarContent = "";
+  // Use express session for if user is logged in, show my trees and login, else opposite
+  if(clickable === null || clickable === "")
+  {
+      navbarContent = '<nav class="navbar">' +
+                      '   <ul>' +
+                      '       <li><a href="#"onclick="navigateToHome()">Home</a></li>' +
+                      '       <li style = color: #ccc; display: none; ><a href="#">My Trees</a></li>' +
+                      '       <li><a href="#"onclick="navigateToEditor()">Tree Editor</a></li>' +
+                      '       <li><a href="#"onclick="navigateToLogin()">Login</a></li>' +
+                      '       <li><a href="#"onclick="navigateToUI()">UI</a></li>' +
+                      '       <li><a href="#"onclick="navigateToAbout()">About</a></li>' +
+                      '   </ul>' +
+                      '</nav>';
+  }
+  else
+  {
+      navbarContent = '<nav class="navbar">' +
+      '   <ul>' +
+      '       <li><a href="#"onclick="navigateToHome()">Home</a></li>' +
+      '       <li ><a href="#" onclick="navigateToMyTrees()">My Trees</a></li>' +
+      '       <li><a href="#"onclick="navigateToEditor()">Tree Editor</a></li>' +
+      '       <li><a href="#"onclick="navigateToLogin()">Login</a></li>' +
+      '        <li><a href="#"onclick="navigateToUI()">UI</a></li>' +
+      '       <li><a href="#"onclick="navigateToAbout()">About</a></li>' +
+      '   </ul>' +
+      '</nav>';
+  }
+      
+  // Design the navbar
+  
+  // Replace the innerHTML with dynamic content
+   navbarPlaceholder.innerHTML = navbarContent;
+}
+
+async function checkLoggedIn() {
+  try {
+  const response = await fetch('index',
+    {
+      method: 'POST',
+      headers:
+      {
+        'Content-Type': 'application/json'
+      },
+      
+    });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const data = await response.json();
+      console.log('Success:', data.message, 'logged in.');
+      try 
+  {
+    return (data.message != null ? data.message : "");
+  }
+  catch
+  {
+    return "";
+  }
+  }
+catch (error) {
+  console.error('Error:', error);
+  throw error;	}
 }
