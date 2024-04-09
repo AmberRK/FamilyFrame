b3bf9bc04f507d5e4ce4be3f57d94dcd71c708f7
Aidan La Penta <alape632@live.kutztown.edu>
2024-04-09 17:58:46 -0400

    Removed old hashing method

--- Project/static/scripts/accCreate.js
+++ Project/static/scripts/accCreate.js
@@ -32,17 +32,5 @@ async function createAcct() {
     else {
       window.alert("Email failed regex");
     };
-    // bcrypt.hash(myPlaintextPassword, saltRounds).then(function (hash) {
-    // Store hash in your password DB.
-    // console.log(hash);
-    // });
-    // const saltRounds = 12;
-    // const plainPass = "testpassworD1"; //THIS IS TEMP FOR TESTING, SHOULD BE USER INPUT
-    // hash(password, saltRounds)
-    //   .then(hash => {
-    //     console.log(`Hash: ${hash}`); //Displays the hash (REMOVE AFTER TESTING)
-    //     // Store hash in your password DB here.
-    //   })
-    //   .catch(err => console.error(err.message));
   });
 };
